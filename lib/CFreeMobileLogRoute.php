d9d3fa1c9b08ef25bfbdf3d89d08fb7f61f77ac2
Cédric Belin <cedric@belin.io>
2014-08-20 03:56:26 +0200

    API calls currently not working using POST method

--- lib/CFreeMobileLogRoute.php
+++ lib/CFreeMobileLogRoute.php
@@ -22,15 +22,6 @@ class CFreeMobileLogRoute extends CLogRoute {
   const END_POINT_URL='https://smsapi.free-mobile.fr/sendmsg';
 
   /**
-   * The maximum length of sent messages.
-   * Defaults to `0`, meaning no length limit.
-   * @property lengthLimit
-   * @type int
-   * @default 0
-   */
-  public $lengthLimit=0;
-
-  /**
    * The identification key associated to the account.
    * @property password
    * @type string
@@ -50,30 +41,22 @@ class CFreeMobileLogRoute extends CLogRoute {
    * @param {array} $logs The list of messages.
    */
   protected function processLogs($logs) {
+    $fields=[
+      'msg'=>mb_convert_encoding($text, 'ISO-8859-1', Yii::app()->charset),
+      'pass'=>$this->password,
+      'user'=>$this->userName
+    ];
+
     $resource=null;
-
-    $text=implode("\n", array_map(function($log) {
-      return $this->formatLogMessage($log[0], $log[1], $log[2], $log[3]);
-    }, $logs));
-
-    if($this->lengthLimit>0 && mb_strlen($text)>$this->lengthLimit)
-      $text=mb_strimwidth($text, 0, $this->lengthLimit, '...');
+    $url=static::END_POINT_URL.'?'.http_build_query($fields, '', '&', PHP_QUERY_RFC3986);
 
     try {
-      $resource=curl_init(static::END_POINT_URL);
+      $resource=curl_init($url);
       if(!$resource) throw new CException('Resource not found.');
-
-      $fields=[
-        'msg'=>mb_convert_encoding($text, 'ISO-8859-1', Yii::app()->charset),
-        'pass'=>$this->password,
-        'user'=>$this->userName
-      ];
 
       if(!curl_setopt_array($resource, [
         CURLOPT_ENCODING=>'',
         CURLOPT_FAILONERROR=>true,
-        CURLOPT_POST=>true,
-        CURLOPT_POSTFIELDS=>$fields,
         CURLOPT_RETURNTRANSFER=>true,
         CURLOPT_TIMEOUT=>5000,
         CURLOPT_SSL_VERIFYPEER=>false
@@ -81,19 +64,10 @@ class CFreeMobileLogRoute extends CLogRoute {
 
       $response=curl_exec($resource);
       if($response===false) throw new CException(curl_error($resource));
-
-      $data=@simplexml_load_string($response);
-      if(!$data) throw new CException('Unable to parse the XML response.');
-
       curl_close($resource);
-      return $data;
     }
 
     catch(CException $e) {
-      Yii::log($e->getMessage(), CLogger::LEVEL_ERROR, 'system.logging.CFreeMobileLogRoute');
-    }
-
-    finally {
       if($resource) curl_close($resource);
     }
   }
