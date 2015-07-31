c3ad04b5c7b855566ed796e1bf2e024d1c6a95c1
Cédric Belin <cedric@belin.io>
2015-08-01 01:30:08 +0200

    Fixed documentation task

--- tool/dist.php
+++ tool/dist.php
@@ -10,5 +10,5 @@ chdir(dirname(__DIR__));
 $archive=new ZipArchive();
 $archive->open('var/free-mobile.yii-0.4.0.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
 $archive->addGlob('*.{json,md,txt}', GLOB_BRACE);
-$archive->addGlob('lib/*.php');
+$archive->addGlob('lib/**/*.php');
 $archive->close();
