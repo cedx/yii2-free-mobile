90191d856d62cba14b1d141cf7a2e89f35a5d833
Cédric Belin <cedric@belin.io>
2016-11-27 02:03:43 +0100

    Added the `Client` component

---
+++ lib/Client.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Implementation of the `yii\freemobile\Client` class.
+ */
+namespace yii\freemobile;
+
+use freemobile\{Client as FreeMobileClient};
+use yii\base\{Component, Exception};
+
+/**
+ * Sends messages by SMS to a [Free Mobile](http://mobile.free.fr) account.
+ */
+class Client extends Component implements \JsonSerializable {
+
+  /**
+   * @var string An event that is triggered when a request is made to the remote service.
+   */
+  const EVENT_REQUEST = 'request';
+
+  /**
+   * @var string An event that is triggered when a response is received from the remote service.
+   */
+  const EVENT_RESPONSE = 'reponse';
+
+  /**
+   * @var string The underlying Free Mobile client.
+   */
+  private $client;
+
+  /**
+   * Initializes a new instance of the class.
+   * @param array $config Name-value pairs that will be used to initialize the object properties.
+   */
+  public function __construct(array $config = []) {
+    $this->client = new FreeMobileClient();
+    parent::__construct($config);
+
+    $this->client->onRequest()->subscribeCallback(function($request) {
+      $this->trigger(static::EVENT_REQUEST, \Yii::createObject(['class' => RequestEvent::class, 'request' => $request]));
+    });
+
+    $this->client->onResponse()->subscribeCallback(function($response) {
+      $this->trigger(static::EVENT_RESPONSE, \Yii::createObject(['class' => ResponseEvent::class, 'response' => $response]));
+    });
+  }
+
+  /**
+   * Returns a string representation of this object.
+   * @return string The string representation of this object.
+   */
+  public function __toString(): string {
+    $json = json_encode($this, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    return static::class." {$json}";
+  }
+
+  /**
+   * Gets the identification key associated to the account.
+   * @return string The identification key associated to the account.
+   */
+  public function getPassword(): string {
+    return $this->client->getPassword();
+  }
+
+  /**
+   * Gets the user name associated to the account.
+   * @return string The user name associated to the account.
+   */
+  public function getUsername(): string {
+    return $this->client->getUsername();
+  }
+
+  /**
+   * Converts this object to a map in JSON format.
+   * @return \stdClass The map in JSON format corresponding to this object.
+   */
+  public function jsonSerialize(): \stdClass {
+    return $this->client->jsonSerialize();
+  }
+
+  /**
+   * Sends a SMS message to the underlying account.
+   * @param string $text The text of the message to send.
+   * @return bool Whether the operation was successful.
+   */
+  public function sendMessage(string $text): bool {
+    $this->client->sendMessage($text)->subscribeCallback(
+      function() use (&$result) { $result = true; },
+      function() use (&$result) { $result = false; }
+    );
+
+    return $result;
+  }
+
+  /**
+   * Sets the identification key associated to the account.
+   * @param string $value The new identification key.
+   * @return Client This instance.
+   */
+  public function setPassword(string $value): self {
+    $this->client->setPassword($value);
+    return $this;
+  }
+
+  /**
+   * Sets the user name associated to the account.
+   * @param string $value The new username.
+   * @return Client This instance.
+   */
+  public function setUsername(string $value): self {
+    $this->client->setUsername($value);
+    return $this;
+  }
+}
---
+++ test/ClientTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Implementation of the `yii\freemobile\test\ClientTest` class.
+ */
+namespace yii\freemobile\test;
+use yii\freemobile\{Client};
+
+/**
+ * Tests the features of the `yii\freemobile\Client` class.
+ */
+class ClientTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Tests the `Client` constructor.
+   */
+  public function testConstructor() {
+    $client = new Client(['username' => 'anonymous', 'password' => 'secret']);
+    $this->assertEquals('secret', $client->getPassword());
+    $this->assertEquals('anonymous', $client->getUsername());
+
+    $this->assertSame($client, $client->setPassword(''));
+    $this->assertEmpty($client->getPassword());
+  }
+
+  /**
+   * Tests the `Client::jsonSerialize()` method.
+   */
+  public function testJsonSerializeoJSON() {
+    $data = (new Client(['username' => 'anonymous', 'password' => 'secret']))->jsonSerialize();
+
+    $this->assertObjectHasAttribute('password', $data);
+    $this->assertEquals('secret', $data->password);
+
+    $this->assertObjectHasAttribute('username', $data);
+    $this->assertEquals('anonymous', $data->username);
+  }
+
+  /**
+   * Tests the `Client::sendMessage()` method.
+   */
+  public function testSendMessage() {
+    $client = new Client(['username' => '', 'password' => '']);
+    $this->assertFalse($client->sendMessage('Hello World!'));
+
+    $client = new Client(['username' => 'anonymous', 'password' => 'secret']);
+    $this->assertFalse($client->sendMessage(''));
+
+    if (is_string($username = getenv('FREEMOBILE_USERNAME')) && is_string($password = getenv('FREEMOBILE_PASSWORD'))) {
+      $client = new Client(['username' => $username, 'password' => $password]);
+      $this->assertTrue($client->sendMessage('Bonjour Cédric !'));
+    }
+  }
+}
