acaa42c9d119b13874cfe0e1622c55504081e5ab
Cédric Belin <cedric@belin.io>
2018-08-05 11:39:50 +0200

    Dropped the dependency on PHPUnit-Expect

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -2,7 +2,6 @@
 declare(strict_types=1);
 namespace yii\freemobile;
 
-use function PHPUnit\Expect\{expect, fail, it};
 use PHPUnit\Framework\{TestCase};
 use Psr\Http\Message\{UriInterface};
 use yii\base\{InvalidArgumentException, InvalidConfigException};
@@ -16,48 +15,45 @@ class ClientTest extends TestCase {
    * @test Client::init
    */
   public function testInit(): void {
-    it('should throw an exception if the username or password is empty', function() {
-      expect(function() { new Client; })->to->throw(InvalidConfigException::class);
-    });
+    // It should throw an exception if the username or password is empty.
+    $this->expectException(InvalidConfigException::class);
+    new Client;
   }
 
   /**
    * @test Client::sendMessage
    */
   public function testSendMessage(): void {
-    it('should not send invalid messages with valid credentials', function() {
-      try {
-        (new Client(['username' => 'anonymous', 'password' => 'secret']))->sendMessage('');
-        fail('An empty message with credentials should not be sent');
-      }
+    // It should not send invalid messages with valid credentials.
+    try {
+      (new Client(['username' => 'anonymous', 'password' => 'secret']))->sendMessage('');
+      $this->fail('An empty message with credentials should not be sent');
+    }
 
-      catch (\Throwable $e) {
-        expect($e)->to->be->an->instanceOf(InvalidArgumentException::class);
-      }
-    });
+    catch (\Throwable $e) {
+      assertThat($e, isInstanceOf(InvalidArgumentException::class));
+    }
 
-    it('should throw a `ClientException` if a network error occurred', function() {
-      try {
-        (new Client(['username' => 'anonymous', 'password' => 'secret', 'endPoint' => 'http://localhost']))->sendMessage('Hello World!');
-        fail('A message with an invalid endpoint should not be sent');
-      }
+    // It should throw a `ClientException` if a network error occurred.
+    try {
+      (new Client(['username' => 'anonymous', 'password' => 'secret', 'endPoint' => 'http://localhost']))->sendMessage('Hello World!');
+      $this->fail('A message with an invalid endpoint should not be sent');
+    }
 
-      catch (\Throwable $e) {
-        expect($e)->to->be->an->instanceOf(ClientException::class);
-      }
-    });
+    catch (\Throwable $e) {
+      assertThat($e, isInstanceOf(ClientException::class));
+    }
 
+    // It should send valid messages with valid credentials.
     if (is_string($username = getenv('FREEMOBILE_USERNAME')) && is_string($password = getenv('FREEMOBILE_PASSWORD'))) {
-      it('should send valid messages with valid credentials', function() use ($password, $username) {
-        try {
-          (new Client(['username' => $username, 'password' => $password]))->sendMessage('Bonjour Cédric !');
-          expect(true)->to->be->true;
-        }
+      try {
+        (new Client(['username' => $username, 'password' => $password]))->sendMessage('Bonjour Cédric !');
+        assertThat(true, isTrue());
+      }
 
-        catch (\Throwable $e) {
-          expect($e)->to->be->an->instanceOf(ClientException::class);
-        }
-      });
+      catch (\Throwable $e) {
+        assertThat($e, isInstanceOf(ClientException::class));
+      }
     }
   }
 
@@ -67,15 +63,13 @@ class ClientTest extends TestCase {
   public function testSetEndPoint(): void {
     $client = new Client(['username' => 'anonymous', 'password' => 'secret']);
 
-    it('should not be empty by default', function() use ($client) {
-      expect($client->endPoint)->to->be->an->instanceOf(UriInterface::class);
-      expect((string) $client->endPoint)->to->equal('https://smsapi.free-mobile.fr');
-    });
+    // It should not be empty by default.
+    assertThat($client->endPoint, isInstanceOf(UriInterface::class));
+    assertThat((string) $client->endPoint, equalTo('https://smsapi.free-mobile.fr'));
 
-    it('should be an instance of the `Uri` class', function() use ($client) {
-      $client->endPoint = 'http://localhost';
-      expect($client->endPoint)->to->be->an->instanceOf(UriInterface::class);
-      expect((string) $client->endPoint)->to->equal('http://localhost');
-    });
+    // It should be an instance of the `Uri` class.
+    $client->endPoint = 'http://localhost';
+    assertThat($client->endPoint, isInstanceOf(UriInterface::class));
+    assertThat((string) $client->endPoint, equalTo('http://localhost'));
   }
 }
--- test/LogTargetTest.php
+++ test/LogTargetTest.php
@@ -2,7 +2,6 @@
 declare(strict_types=1);
 namespace yii\freemobile;
 
-use function PHPUnit\Expect\{expect, it};
 use PHPUnit\Framework\{TestCase};
 use yii\base\{InvalidConfigException};
 use yii\log\{Logger};
@@ -16,37 +15,47 @@ class LogTargetTest extends TestCase {
    * @test LogTarget::formatMessage
    */
   public function testFormatMessage(): void {
-    it('should return a formatted message including the log level and category', function() {
-      $message = ['Hello World!', Logger::LEVEL_ERROR, 'tests', time()];
-      expect((new LogTarget)->formatMessage($message))->to->equal('[tests] Hello World!');
-    });
+    // It should return a formatted message including the log level and category.
+    $message = ['Hello World!', Logger::LEVEL_ERROR, 'tests', time()];
+    assertThat((new LogTarget)->formatMessage($message), equalTo('[tests] Hello World!'));
   }
 
   /**
    * @test LogTarget::init
    */
   public function testInit(): void {
-    it('should throw an exception if the client is empty', function() {
+    // It should throw an exception if the client is empty.
+    try {
       \Yii::$app->set('freemobile', null);
-      expect(function() { new LogTarget; })->to->throw(InvalidConfigException::class);
-    });
+      new LogTarget;
+      $this->fail('Exception not thrown.');
+    }
 
-    it('should not throw an exception if the client is not empty', function() {
+    catch (\Throwable $e) {
+      assertThat($e, isInstanceOf(InvalidConfigException::class));
+    }
+
+    // It should not throw an exception if the client is not empty.
+    try {
       \Yii::$app->set('freemobile', new Client(['username' => 'anonymous', 'password' => 'secret']));
-      expect(function() { new LogTarget; })->to->not->throw;
-    });
+      new LogTarget;
+    }
 
-    it('should allow a customized client component ID', function() {
-      \Yii::$app->set('freemobileTest', [
-        'class' => Client::class,
-        'username' => 'anonymous',
-        'password' => 'secret'
-      ]);
+    catch (\Throwable $e) {
+      $this->fail($e->getMessage());
+    }
 
-      expect((new LogTarget(['client' => 'freemobileTest']))->client)
-        ->to->be->an->instanceOf(Client::class)
-        ->and->be->identicalTo(\Yii::$app->get('freemobileTest'));
-    });
+    // It should allow a customized client component ID.
+    \Yii::$app->set('freemobileTest', [
+      'class' => Client::class,
+      'username' => 'anonymous',
+      'password' => 'secret'
+    ]);
+
+    assertThat((new LogTarget(['client' => 'freemobileTest']))->client, logicalAnd(
+      isInstanceOf(Client::class),
+      identicalTo(\Yii::$app->get('freemobileTest'))
+    ));
   }
 
   /**
