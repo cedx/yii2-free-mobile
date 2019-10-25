6c141403a7ebfcb883f80f61cb57500227f85316
Cédric Belin <cedric@belin.io>
2019-10-25 11:53:19 +0200

    Using arrow functions

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -12,30 +12,25 @@ class ClientTest extends TestCase {
   /** @testdox ->init() */
   function testInit(): void {
     it('should throw an exception if the username or password is empty', function() {
-      expect(function() { new Client; })->to->throw(InvalidConfigException::class);
+      expect(fn() => new Client)->to->throw(InvalidConfigException::class);
     });
   }
 
   /** @testdox ->sendMessage() */
   function testSendMessage(): void {
     it('should not send invalid messages with valid credentials', function() {
-      expect(function() {
-        (new Client(['username' => 'anonymous', 'password' => 'secret']))->sendMessage('');
-      })->to->throw(InvalidArgumentException::class);
+      $config = ['username' => 'anonymous', 'password' => 'secret'];
+      expect(fn() => (new Client($config))->sendMessage(''))->to->throw(InvalidArgumentException::class);
     });
 
     it('should throw a `ClientException` if a network error occurred', function() {
-      expect(function() {
-        $config = ['username' => 'anonymous', 'password' => 'secret', 'endPoint' => new Uri('http://localhost/')];
-        (new Client($config))->sendMessage('Hello World!');
-      })->to->throw();
+      $config = ['username' => 'anonymous', 'password' => 'secret', 'endPoint' => new Uri('http://localhost/')];
+      expect(fn() => (new Client($config))->sendMessage('Hello World!'))->to->throw();
     });
 
     it('should send valid messages with valid credentials', function() {
-      expect(function() {
-        $config = ['username' => getenv('FREEMOBILE_USERNAME'), 'password' => getenv('FREEMOBILE_PASSWORD')];
-        (new Client($config))->sendMessage('Bonjour Cédric, à partir du Yii Framework !');
-      })->to->not->throw;
+      $config = ['username' => getenv('FREEMOBILE_USERNAME'), 'password' => getenv('FREEMOBILE_PASSWORD')];
+      expect(fn() => (new Client($config))->sendMessage('Bonjour Cédric, à partir du Yii Framework !'))->to->not->throw;
     });
   }
 }
