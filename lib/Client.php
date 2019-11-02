52120bd371e9e6690e2685d86084770a915e947d
Cédric Belin <cedric@belin.io>
2019-11-02 21:36:13 +0100

    Added a getter/setter for the `endPoint` property

--- lib/Client.php
+++ lib/Client.php
@@ -8,7 +8,10 @@ use yii\base\{Component, InvalidArgumentException, InvalidConfigException};
 use yii\httpclient\{Client as HttpClient, CurlTransport};
 use yii\web\{HttpException};
 
-/** Sends messages by SMS to a Free Mobile account. */
+/**
+ * Sends messages by SMS to a Free Mobile account.
+ * @property UriInterface $endPoint The URL of the API end point.
+ */
 class Client extends Component {
 
   /** @var string An event that is triggered when a request is made to the remote service. */
@@ -17,14 +20,14 @@ class Client extends Component {
   /** @var string An event that is triggered when a response is received from the remote service. */
   const eventResponse = 'response';
 
-  /** @var UriInterface The URL of the API end point. */
-  public UriInterface $endPoint;
-
   /** @var string The identification key associated to the account. */
   public string $password = '';
 
   /** @var string The user name associated to the account. */
   public string $username = '';
+
+  /** @var UriInterface The URL of the API end point. */
+  private UriInterface $endPoint;
 
   /** @var HttpClient The underlying HTTP client. */
   private HttpClient $httpClient;
@@ -34,10 +37,19 @@ class Client extends Component {
    * @param array $config Name-value pairs that will be used to initialize the object properties.
    */
   function __construct(array $config = []) {
+    $this->endPoint = new Uri('https://smsapi.free-mobile.fr/');
     $this->httpClient = new HttpClient(['transport' => CurlTransport::class]);
     $this->httpClient->on(HttpClient::EVENT_BEFORE_SEND, fn($event) => $this->trigger(static::eventRequest, $event));
     $this->httpClient->on(HttpClient::EVENT_AFTER_SEND, fn($event) => $this->trigger(static::eventResponse, $event));
     parent::__construct($config);
+  }
+
+  /**
+   * Gets the URL of the API end point.
+   * @return UriInterface The URL of the API end point.
+   */
+  function getEndPoint(): UriInterface {
+    return $this->endPoint;
   }
 
   /**
@@ -46,11 +58,7 @@ class Client extends Component {
    */
   function init(): void {
     parent::init();
-    if (!mb_strlen($this->username) || !mb_strlen($this->password)) throw new InvalidConfigException('The account credentials are invalid');
-
-    /** @var UriInterface|null $endPoint */
-    $endPoint = $this->endPoint;
-    if (!$endPoint) $this->endPoint = new Uri('https://smsapi.free-mobile.fr/');
+    if (!mb_strlen($this->username) || !mb_strlen($this->password)) throw new InvalidConfigException('The account credentials are invalid.');
   }
 
   /**
@@ -61,7 +69,7 @@ class Client extends Component {
    */
   function sendMessage(string $text): void {
     $message = trim($text);
-    if (!mb_strlen($message)) throw new InvalidArgumentException('The specified message is empty');
+    if (!mb_strlen($message)) throw new InvalidArgumentException('The specified message is empty.');
 
     $uri = UriResolver::resolve($this->endPoint, new Uri('sendmsg'))->withQuery(build_query([
       'msg' => mb_substr($message, 0, 160),
@@ -77,5 +85,15 @@ class Client extends Component {
     catch (\Throwable $e) {
       throw new ClientException($e->getMessage(), $uri, $e);
     }
+  }
+
+  /**
+   * Sets the URL of the API end point.
+   * @param string|UriInterface $value The new URL.
+   * @return $this This instance.
+   */
+  function setEndPoint($value): self {
+    $this->endPoint = new Uri((string) $value);
+    return $this;
   }
 }
